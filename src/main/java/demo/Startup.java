cd4e36a3e9e1dfed94978432efb158b9b55e8a90
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-11-18 20:38:13 -0800

    Got Sign in with Slack token exchange working

--- src/main/java/demo/Startup.java
+++ src/main/java/demo/Startup.java
@@ -5,29 +5,89 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class Startup {
 
   public static void main(String[] args) {
     try {
       runUntilShutdown(args[0]);
-    } catch (IOException e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
-  public static void runUntilShutdown(String clientSecret) throws IOException {
+  public static void runUntilShutdown(String clientSecret) throws IOException, InterruptedException {
     HttpServer server = HttpServer.create(new InetSocketAddress(8080), 64);
     server.createContext("/", Startup::landingPageRequest);
+    server.createContext("/openid/callback/", Startup::openIdCallback);
     server.start();
   }
 
+  private static void openIdCallback(HttpExchange exchange) throws IOException {
+    System.out.println("/openid/callback/ request received");
+    printExchangeRequestInfo(exchange);
+    String code = exchange.getRequestURI().toString().split("code=")[1];
+    System.out.println("code: " + code);
+    URI url = getOpenIdConnectUrl(code);
+    System.out.println("request URL is: " + url.toString());
+    HttpRequest request = HttpRequest.newBuilder(url)
+        .POST(BodyPublishers.noBody())
+        .header("Content-type","application/x-www-form-urlencoded")
+        .build();
+    HttpClient client = HttpClient.newHttpClient();
+    try {
+      System.out.println("Sending request for token");
+      HttpResponse<String> response = client.send(request,
+          BodyHandlers.ofString(StandardCharsets.US_ASCII));
+      System.out.println("Received response:");
+      System.out.println(response.body());
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static URI getOpenIdConnectUrl(String code) {
+    try {
+      return new URI("https://slack.com/api/openid.connect.token/?client_id=2464212157.2680821785088"
+          + "&client_secret=96e6d39590a94ecf1eb3f7f300d622c5"
+          + "&code=" + code
+          + "&redirect_uri=https://3c04-2600-1700-2320-ab30-acc3-c067-4009-5d2b.ngrok.io/openid/callback/");
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
   private static void landingPageRequest(HttpExchange exchange) throws IOException {
-    String body = "Login link goes here: ";
+    System.out.println("Root request received");
+    printExchangeRequestInfo(exchange);
+    String body = slackButtonHtml();
     byte[] bodyBytes = body.getBytes(StandardCharsets.US_ASCII);
     exchange.sendResponseHeaders(200, bodyBytes.length);
     OutputStream os = exchange.getResponseBody();
     os.write(bodyBytes);
+  }
+
+  private static void printExchangeRequestInfo(HttpExchange exchange) throws IOException {
+    System.out.println("Request received");
+    System.out.println(exchange.getRequestMethod() + exchange.getRequestURI());
+    System.out.println("Headers");
+    exchange.getRequestHeaders().forEach((k, v) -> System.out.println(k + " " + v));
+    System.out.println("Body");
+    byte[] bodyBytes = exchange.getRequestBody().readAllBytes();
+    System.out.println(new String(bodyBytes, StandardCharsets.US_ASCII));
+  }
+
+  private static String slackButtonHtml() {
+    return "<a href=\"https://slack.com/openid/connect/authorize?scope=openid%20email%20profile&amp;response_type=code&amp;redirect_uri=https%3A%2F%2F3c04-2600-1700-2320-ab30-acc3-c067-4009-5d2b.ngrok.io%2Fopenid%2Fcallback%2F&amp;client_id=2464212157.2680821785088\" style=\"align-items:center;color:#000;background-color:#fff;border:1px solid #ddd;border-radius:4px;display:inline-flex;font-family:Lato, sans-serif;font-size:16px;font-weight:600;height:48px;justify-content:center;text-decoration:none;width:256px\"><svg xmlns=\"http://www.w3.org/2000/svg\" style=\"height:20px;width:20px;margin-right:12px\" viewBox=\"0 0 122.8 122.8\"><path d=\"M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z\" fill=\"#e01e5a\"></path><path d=\"M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z\" fill=\"#36c5f0\"></path><path d=\"M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z\" fill=\"#2eb67d\"></path><path d=\"M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z\" fill=\"#ecb22e\"></path></svg>Sign in with Slack</a>";
   }
 }
