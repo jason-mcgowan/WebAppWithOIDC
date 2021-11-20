375d5aae71a041bdd37d4832a0e5dda5444b2d5a
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-11-19 17:38:06 -0800

    Exchange for token with Slack working

---
+++ src/main/java/demo/IdToken.java
@@ -0,0 +1,99 @@
+package demo;
+
+public class IdToken {
+  private String iss;
+  private String sub;
+  private String aud;
+  private String exp;
+  private String iat;
+  private String auth_time;
+  private String nonce;
+  private String at_hash;
+  private String httpsslackcomteam_id;
+  private String httpsslackcomuser_id;
+  private String email;
+  private String email_verified;
+  private String date_email_verified;
+  private String locale;
+  private String name;
+  private String given_name;
+  private String family_name;
+  private String httpsslackcomteam_image_230;
+  private String httpsslackcomteam_image_default;
+
+  public String getIss() {
+    return iss;
+  }
+
+  public String getSub() {
+    return sub;
+  }
+
+  public String getAud() {
+    return aud;
+  }
+
+  public String getExp() {
+    return exp;
+  }
+
+  public String getIat() {
+    return iat;
+  }
+
+  public String getAuth_time() {
+    return auth_time;
+  }
+
+  public String getNonce() {
+    return nonce;
+  }
+
+  public String getAt_hash() {
+    return at_hash;
+  }
+
+  public String getHttpsslackcomteam_id() {
+    return httpsslackcomteam_id;
+  }
+
+  public String getHttpsslackcomuser_id() {
+    return httpsslackcomuser_id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getEmail_verified() {
+    return email_verified;
+  }
+
+  public String getDate_email_verified() {
+    return date_email_verified;
+  }
+
+  public String getLocale() {
+    return locale;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getGiven_name() {
+    return given_name;
+  }
+
+  public String getFamily_name() {
+    return family_name;
+  }
+
+  public String getHttpsslackcomteam_image_230() {
+    return httpsslackcomteam_image_230;
+  }
+
+  public String getHttpsslackcomteam_image_default() {
+    return httpsslackcomteam_image_default;
+  }
+}
