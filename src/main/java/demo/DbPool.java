71b39bb9c0373bb8c34180fbc5426fdffe5492e3
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-12-02 18:15:07 -0800

    Chained dbpool constructors

--- src/main/java/demo/DbPool.java
+++ src/main/java/demo/DbPool.java
@@ -3,7 +3,6 @@ package demo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -22,9 +21,7 @@ public class DbPool {
   }
 
   public DbPool(Config config) {
-    this.url = config.getDb_address();
-    this.user = config.getDb_username();
-    this.password = config.getDb_password();
+    this(config.getDb_address(), config.getDb_username(), config.getDb_password());
   }
 
   public PreparedStatement executeQuery(String sql) throws SQLException {
