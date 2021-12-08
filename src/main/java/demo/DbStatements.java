d8ba1b5131403572db0e0a82fa1a62abe952ab9c
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-12-07 20:32:16 -0800

    Implemented ticket purchases

--- src/main/java/demo/DbStatements.java
+++ src/main/java/demo/DbStatements.java
@@ -25,8 +25,14 @@ public class DbStatements {
       "UPDATE local_user SET display_name = ? WHERE id = ?";
   private static final String INSERT_EVENT_STATEMENT =
       "INSERT INTO event VALUES (0, ?, ?, ?, ?, ?, ?, ?)";
-  private static final String EVENT_QUERY =
+  private static final String ALL_EVENTS_QUERY =
       "SELECT * FROM event";
+  private static final String EVENT_ID_QUERY =
+      "SELECT * FROM event WHERE id=?";
+  private static final String LOWER_TICKET_COUNT =
+      "UPDATE event SET quantity_remaining=quantity_remaining-? WHERE id=?";
+  private static final String INSERT_PURCHASE =
+      "INSERT INTO purchase VALUES (0, ?, ?, ?)";
 
   public static Map<Integer, String> slackSubQuery(String sub) throws SQLException {
     Map<Integer, String> result = new HashMap<>();
@@ -104,7 +110,7 @@ public class DbStatements {
   public static Collection<EventData> getEvents() throws SQLException {
     Connection conn = pool.getConnection();
     Collection<EventData> events = new LinkedList<>();
-    try (PreparedStatement ps = conn.prepareStatement(EVENT_QUERY,
+    try (PreparedStatement ps = conn.prepareStatement(ALL_EVENTS_QUERY,
         ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
       ResultSet rs = ps.executeQuery();
       while (rs.next()) {
@@ -120,10 +126,49 @@ public class DbStatements {
     EventData event = new EventData();
     event.setId(rs.getInt(1));
     event.setName(rs.getString(2));
+    event.setDescription(rs.getString(3));
     event.setStartDate(rs.getDate(4));
     event.setEndDate(rs.getDate(5));
     event.setQuantity(rs.getInt(7));
     event.setPrice(rs.getBigDecimal(8));
     return event;
+  }
+
+  public static EventData getEvent(int event) throws SQLException {
+    Connection conn = pool.getConnection();
+    try (PreparedStatement ps = conn.prepareStatement(EVENT_ID_QUERY,
+        ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE)) {
+      ps.setInt(1, event);
+      ResultSet rs = ps.executeQuery();
+      rs.next();
+      if (rs.isAfterLast()) {
+        return null;
+      }
+      return getEventData(rs);
+    } finally {
+      pool.returnConnection(conn);
+    }
+  }
+
+  public static void purchaseTickets(int quantity, int eventId, int userId) throws SQLException {
+    Connection conn = pool.getConnection();
+    try (
+        PreparedStatement lowerTicket = conn.prepareStatement(LOWER_TICKET_COUNT,
+            ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        PreparedStatement insertPurchase = conn.prepareStatement(INSERT_PURCHASE,
+            ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)) {
+      lowerTicket.setInt(1, quantity);
+      lowerTicket.setInt(2, eventId);
+      lowerTicket.executeUpdate();
+      insertPurchase.setInt(1, userId);
+      insertPurchase.setInt(2, eventId);
+      insertPurchase.setInt(3, quantity);
+      insertPurchase.executeUpdate();
+    }catch (SQLException e) {
+      conn.rollback();
+      throw e;
+    } finally {
+      pool.returnConnection(conn);
+    }
   }
 }
--- src/main/java/demo/ViewEventHandler.java
+++ src/main/java/demo/ViewEventHandler.java
@@ -9,6 +9,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
+import util.HttpTools;
 
 public class ViewEventHandler implements HttpHandler {
 
@@ -20,6 +21,99 @@ public class ViewEventHandler implements HttpHandler {
 
   @Override
   public void handle(HttpExchange exchange) throws IOException {
+    int eventId = getEvent(exchange);
+    if (eventId == 0) {
+      listEvents(exchange);
+    } else {
+      handleSubEvent(eventId, exchange);
+    }
+  }
+
+  private void handleSubEvent(int eventId, HttpExchange exchange) throws IOException {
+    EventData event;
+    try {
+      event = DbStatements.getEvent(eventId);
+    } catch (SQLException e) {
+      sendErrorMessage(exchange);
+      return;
+    }
+    if (event == null) {
+      sendErrorMessage(exchange);
+      return;
+    }
+    if (exchange.getRequestMethod().equals("POST")) {
+      doPost(event, exchange);
+    } else {
+      doShow(event, exchange);
+    }
+
+  }
+
+  private void doShow(EventData event, HttpExchange exchange) throws IOException {
+    Map<String, Object> model = new HashMap<>();
+    String templatePath = "/events/show.html";
+    model.put("mainUrl", "/");
+    model.put("event", event);
+    try {
+      ExchangeTools.templateResponse(exchange, model, templatePath);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void doPost(EventData eventData, HttpExchange exchange) throws IOException {
+    String payload = new String(exchange.getRequestBody().readAllBytes());
+    Map<String, String> pairs;
+    try {
+      pairs = HttpTools.parseUrlEncodedPostPayload(payload);
+    } catch (IllegalArgumentException e) {
+      sendErrorMessage(exchange);
+      return;
+    }
+    String quantityString = pairs.get("quantity");
+    int quantity;
+    try {
+      quantity = Integer.parseInt(quantityString);
+    } catch (NumberFormatException e) {
+      sendErrorMessage(exchange);
+      return;
+    }
+    if (quantity < 0 || quantity > eventData.getQuantity()) {
+      sendErrorMessage(exchange);
+      return;
+    }
+    SessionData sd = (SessionData) exchange.getAttribute(SessionFilter.SESSION_DATA_ATT);
+    try {
+      DbStatements.purchaseTickets(quantity, eventData.getId(), sd.getLocalUserId());
+    } catch (SQLException e) {
+      e.printStackTrace();
+      sendErrorMessage(exchange);
+      return;
+    }
+    Map<String, Object> model = new HashMap<>();
+    String templatePath = "/message.html";
+    model.put("mainUrl", "/");
+    model.put("message", "Purchase complete");
+    try {
+      ExchangeTools.templateResponse(exchange, model, templatePath);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void sendErrorMessage(HttpExchange exchange) throws IOException {
+    Map<String, Object> model = new HashMap<>();
+    String templatePath = "/message.html";
+    model.put("mainUrl", "/");
+    model.put("message", "Error looking up event");
+    try {
+      ExchangeTools.templateResponse(exchange, model, templatePath);
+    } catch (TemplateException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void listEvents(HttpExchange exchange) throws IOException {
     Collection<EventData> events;
     try {
       events = DbStatements.getEvents();
@@ -36,5 +130,18 @@ public class ViewEventHandler implements HttpHandler {
     } catch (TemplateException e) {
       e.printStackTrace();
     }
+  }
+
+  private int getEvent(HttpExchange exchange) {
+    String path = exchange.getRequestURI().getPath();
+    String subPath = path.substring(config.getEventsPath().length());
+    String eventStr = subPath.replaceAll("/", "");
+    int event;
+    try {
+      event = Integer.parseInt(eventStr);
+    } catch (NumberFormatException e) {
+      event = 0;
+    }
+    return event;
   }
 }
