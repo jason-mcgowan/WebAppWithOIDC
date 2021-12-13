92529287706c93c4699b07e42340c5a6b5f7e603
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-12-12 20:06:18 -0800

    Added stop method to server

--- src/main/java/Network/Server.java
+++ src/main/java/Network/Server.java
@@ -10,55 +10,60 @@ import Network.Handlers.SlackOidcHandler;
 import Network.Handlers.TransactionsHandler;
 import Network.Handlers.TransferHandler;
 import Network.Handlers.ViewEventHandler;
+import com.sun.net.httpserver.Filter;
 import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import common.Config;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Server {
+
+  private HttpServer server;
 
   public void start(Config config) throws IOException {
     ConcurrentMap<String, SessionData> sessions = new ConcurrentHashMap<>();
     LoginCheckFilter loginCheckFilter = new LoginCheckFilter(
         config.getWebHost() + config.getLoginPath());
     SessionFilter sessionFilter = new SessionFilter(sessions);
+    List<Filter> standardFilters = List.of(sessionFilter, loginCheckFilter);
 
-    HttpServer server = HttpServer.create(new InetSocketAddress(config.getServerPort()), -1);
+    server = HttpServer.create(new InetSocketAddress(config.getServerPort()), -1);
     server.createContext(config.getLoginPath(), new LoginHandler(config)).getFilters()
         .add(sessionFilter);
     server.createContext(config.getSlackCallbackUrl(), new SlackOidcHandler()).getFilters()
         .add(sessionFilter);
-    HttpContext context = server.createContext("/", new RootHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
 
-    context = server.createContext(config.getAccountPath(), new AccountHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
-
-    context = server.createContext(config.getCreateEventPath(), new CreateEventHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
-
-    context = server.createContext(config.getLogoutPath(), new LogoutHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
-
-    context = server.createContext(config.getEventsPath(), new ViewEventHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
-
-    context = server.createContext(config.getTransactionsPath(), new TransactionsHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
-
-    context = server.createContext(config.getTransferPath(), new TransferHandler(config));
-    context.getFilters().add(sessionFilter);
-    context.getFilters().add(loginCheckFilter);
+    setupHandlerFilters(server, "/", new RootHandler(config), standardFilters);
+    setupHandlerFilters(server, config.getAccountPath(), new AccountHandler(config),
+        standardFilters);
+    setupHandlerFilters(server, config.getCreateEventPath(), new CreateEventHandler(config),
+        standardFilters);
+    setupHandlerFilters(server, config.getLogoutPath(), new LogoutHandler(config),
+        standardFilters);
+    setupHandlerFilters(server, config.getEventsPath(), new ViewEventHandler(config),
+        standardFilters);
+    setupHandlerFilters(server, config.getTransactionsPath(), new TransactionsHandler(config),
+        standardFilters);
+    setupHandlerFilters(server, config.getTransferPath(), new TransferHandler(config),
+        standardFilters);
 
     server.start();
+  }
+
+  public void stop() {
+    server.stop(0);
+  }
+
+  private void setupHandlerFilters(HttpServer server, String path, HttpHandler handler,
+      List<Filter> filters) {
+    HttpContext context = server.createContext(path, handler);
+    for (Filter filter : filters) {
+      context.getFilters().add(filter);
+    }
   }
 }
